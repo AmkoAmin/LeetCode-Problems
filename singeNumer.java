feb0159920ee0f09c0b10ed944f317d5760ce386
AmkoAmin <146035902+AmkoAmin@users.noreply.github.com>
2023-09-25 23:53:06 +0200

    Add files via upload

---
+++ singeNumer.java
@@ -0,0 +1,31 @@
+public class singeNumer{
+   public int singleNumber(int[] nums){
+      
+      int temp;
+      
+      ArrayList<Integer> list = new ArrayList<>(); 
+      
+      for (int i = 0; i < nums.length -2; i++) {
+         temp = 1;
+         for (int k = i +1; k < nums.length; k++) {
+            
+            if (nums[i] == nums[k]) {
+               temp++;
+            }
+            
+            if (temp == 3) {
+               list.add(nums[i]);
+            }
+            
+         }
+      }
+      
+      for (int i = 0; i < nums.length; i++) {
+         if (!list.contains(nums[i])) {
+            return nums[i];
+         } 
+      }
+      
+      return 0;
+   }
+}
